314d7bc9d7399dee5551d07d87074e707655c569
Scott Miller <scott@scottmillerrealestate.com>
2020-02-15 12:58:24 -0500

    Express Generator API

---
+++ routes/campsiteRouter.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const campsiteRouter = express.Router();
+
+campsiteRouter.use(bodyParser.json());
+
+campsiteRouter.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send all the campsites to you');
+})
+.post((req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+})
+.delete((req, res) => {
+    res.end('Deleting all campsites');
+});
+
+// task 1
+
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send all the campsites to you');
+})
+.post((req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+})
+.delete((req, res) => {
+    res.end('Deleting all campsites');
+});
+
+// end of task 1
+
+module.exports = campsiteRouter;
---
+++ routes/promotionsRouter.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const promotionsRouter = express.Router();
+
+promotionsRouter.use(bodyParser.json());
+
+// task 2
+
+promotionsRouter.route('./promotionsRouter')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send all the campsites to you');
+})
+.post((req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+})
+.delete((req, res) => {
+    res.end('Deleting all campsites');
+});
+
+promotionsRouter.route('/promotions/:promotionId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send all the campsites to you');
+})
+.post((req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+})
+.delete((req, res) => {
+    res.end('Deleting all campsites');
+});
+
+module.exports = promotionsRouter;
