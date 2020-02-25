04f117a55b5dd745c6bebe3288a8b751479fc9f4
Scott Miller <scott@scottmillerrealestate.com>
2020-02-24 19:59:27 -0500

    Express Rest API with MongoDB and Mongoose Part 1

--- app.js
+++ app.js
@@ -8,6 +8,20 @@ const campsiteRouter = require('./routes/campsiteRouter');
 const promotionsRouter = require('./routes/promotionsRouter');
 const partnerRouter = require('./routes/partnerRouter');
 
+const mongoose = require('mongoose');
+
+const url = 'mongodb://localhost:27017/nucampsite';
+const connect = mongoose.connect(url, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
+
+connect.then(() => console.log('Connected correctly to server'), 
+    err => console.log(err)
+);
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
